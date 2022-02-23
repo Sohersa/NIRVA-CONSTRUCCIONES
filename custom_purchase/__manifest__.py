ddea55231317abf8ef61c5043e2f84487b4b66ff
Adán Sohersa <a.gonzalez@gruposohersa.com>
2022-02-23 16:29:50 -0600

    Enable default views

--- custom_purchase/__manifest__.py
+++ custom_purchase/__manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
+    'name': "custom_compras",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        The module's purpose is to create a custom implementation for the existing purchase app.
     """,
 
-    'author': "My Company",
+    'author': "Oupp",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'purchase.requisition'],
 
     # always loaded
     'data': [
