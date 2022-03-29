eeb88078b1c0b9e53b618e169f01dee993c46017
Adán Sohersa <a.gonzalez@gruposohersa.com>
2022-03-29 15:45:29 -0600

    Empresas field

--- custom_purchase/models/models.py
+++ custom_purchase/models/models.py
@@ -3,16 +3,9 @@
 from odoo import models, fields, api
 
 class PurchaseOrder(models.Model):
-    # _name = 'my_module.custom_purchase'
+    
     _inherit='purchase.order'
 
-    custom = fields.Char(default="Campo personalizado heredado")
+    enterprise_domain = [('company_type', '=', 'company')]
 
-    # name = fields.Char()
-    # value = fields.Integer()
-    # value2 = fields.Float(compute="_value_pc", store=True)
-    # description = fields.Text()
-
-    # @api.depends('value')
-    # def _value_pc(self):
-    #     self.value2 = float(self.value) / 100
+    empresa_id = fields.Many2one('res.partner', 'Empresa', domain=enterprise_domain)
