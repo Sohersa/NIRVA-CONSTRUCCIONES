000ee156d8f4eb051f0ea1731d2fa081a2f0697d
Adán Sohersa <a.gonzalez@gruposohersa.com>
2022-03-30 10:39:14 -0600

    Dinamic domain - test

--- custom_purchase/models/models.py
+++ custom_purchase/models/models.py
@@ -3,16 +3,27 @@
 from odoo import models, fields, api
 
 class PurchaseOrder(models.Model):
-    # _name = 'my_module.custom_purchase'
+
     _inherit='purchase.order'
 
     enterprise_domain = [('company_type', '=', 'company')]
     empresa_id = fields.Many2one('res.partner', string='Empresa', domain=enterprise_domain)
-    # name = fields.Char()
-    # value = fields.Integer()
-    # value2 = fields.Float(compute="_value_pc", store=True)
-    # description = fields.Text()
 
+    def _get_child_partners(self):
+        domain =[('id', '=', -1)]
+        partners_list=[]
+        partners = self.env['res.partner'].search([('parent_id','=',self.empresa_id)])
+        for each in partners:
+            partners_list.append(each.id)
+        if partners_list:
+            domain =[('id', 'in', partners_list)]
+            return domain
+        return domain
+
+    @api.onchange('empresa_id')
+    def _onchange_empresa(self):
+        for rec in self:
+            return {'domain': {'partner_id': [('parent_id', '=', rec.partner_id.id)]}}
     # @api.depends('value')
     # def _value_pc(self):
     #     self.value2 = float(self.value) / 100
