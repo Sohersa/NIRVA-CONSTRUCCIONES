ebc5bd9e2c3776347ce3924af1ae36215b5f7af1
Adán Sohersa <a.gonzalez@gruposohersa.com>
2022-04-09 00:16:19 -0500

    Hr depends

--- custom_purchase/__manifest__.py
+++ custom_purchase/__manifest__.py
@@ -2,13 +2,9 @@
 {
     'name': "Purchase customizations",
 
-    'summary': """
-        'Purchase' customizations according to the Nirva logic.
-    """,
+    'summary': """'Purchase' customizations according to the Nirva logic.""",
 
-    'description': """
-        The module's purpose is to create a custom implementation for the existing purchase app.
-    """,
+    'description': """The module's purpose is to create a custom implementation for the existing purchase app.""",
 
     'author': "Oupp",
     'website': "https://www.sohersabim.com/",
@@ -17,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.0.1',
+    'version': '0.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'purchase', 'hr'],
