5c1778b5620e4f9c480bf546149b840f6c40e59d
Adán Sohersa <a.gonzalez@gruposohersa.com>
2022-04-19 15:24:28 -0500

    Purchase requisition form v4 - hotfix 2

--- nirva_purchase/models/purchase_requisition.py
+++ nirva_purchase/models/purchase_requisition.py
@@ -7,14 +7,6 @@ class PurchaseRequisition(models.Model):
     _inherit='purchase.requisition'
 
     @api.onchange('x_studio_obra')
-    # Limitamos el dominio del campo purchase_requisition.x_studio_obra [Obra]
-    def _set_picking_type_domain(self):
-        for rec in self:
-            # Definimos el dominio
-            obra_domain = ["&",("code","=","incoming"),"|",("warehouse_id","!=",False),("warehouse_id.company_id","=", rec.env.company.id)]
-            return {'domain': {'x_studio_obra': obra_domain}}
-
-    @api.onchange('x_subcontrato')
     # Filtramos el dominio del campo purchase_requisition.x_subcontrato [Concepto]
     def _set_stock_location_domain(self):
         for rec in self:
@@ -22,8 +14,11 @@ class PurchaseRequisition(models.Model):
             ubicaciones_domain = [('location_id.name', "ilike", rec.x_studio_obra.wharehouse_id.name)]
             return {'domain': {'x_subcontrato': ubicaciones_domain}}
 
-    overwrite_obra_domain = ["&",("code","=","incoming"),"|",("warehouse_id","!=",False),("warehouse_id.company_id","=", self.env.company.id)]
-    x_studio_obra = fields.Many2one('stock.picking.type', string='Obra actualizadaaaa', domain=overwrite_obra_domain)
+    @api.multi
+    def _overwrite_obra_domain(self): 
+        return ["&",("code","=","incoming"),"|",("warehouse_id","!=",False),("warehouse_id.company_id","=", self.env.company.id)]
+
+    x_studio_obra = fields.Many2one('stock.picking.type', string='Obra actualizadaaaa', domain=_overwrite_obra_domain)
     x_subcontrato = fields.Many2one('stock.location', string='Concepto actualizadooo', domain=[])
 
 
