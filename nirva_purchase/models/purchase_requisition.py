9157bbb3185908d5b1ebf6d2225c92c75266cccf
Adán Sohersa <a.gonzalez@gruposohersa.com>
2022-04-19 14:57:08 -0500

    Purchase requisition form v3

--- nirva_purchase/models/purchase_requisition.py
+++ nirva_purchase/models/purchase_requisition.py
@@ -6,18 +6,25 @@ class PurchaseRequisition(models.Model):
 
     _inherit='purchase.requisition'
 
-    # Limitamos el dominio del campo purchase_requisition.x_studio_obra [Obra]
     @api.onchange('x_studio_obra')
+    # Limitamos el dominio del campo purchase_requisition.x_studio_obra [Obra]
     def _set_picking_type_domain(self):
         for rec in self:
             # Definimos el dominio
             obra_domain = ["&",("code","=","incoming"),"|",("warehouse_id","!=",False),("warehouse_id.company_id","=", rec.env.company.id)]
             return {'domain': {'x_studio_obra': obra_domain}}
 
-    # Filtramos el dominio del campo purchase_requisition.x_subcontrato [Concepto]
     @api.onchange('x_subcontrato')
+    # Filtramos el dominio del campo purchase_requisition.x_subcontrato [Concepto]
     def _set_stock_location_domain(self):
         for rec in self:
             #Definimos el dominio
             ubicaciones_domain = [('location_id.name', "ilike", rec.x_studio_obra.wharehouse_id.name)]
             return {'domain': {'x_subcontrato': ubicaciones_domain}}
+
+    x_studio_obra = fields.Many2one('stock.picking.type', string='Obra actualizadaaaa', domain=_set_picking_type_domain)
+    x_subcontrato = fields.Many2one('stock.location', string='Concepto actualizadooo', domain=_set_stock_location_domain)
+
+
+    # def _on_obra_change(self):
+    # _set_picking_type_domain()
