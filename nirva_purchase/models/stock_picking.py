286330b270777e87904e6f4c29861e4fdcc0c012
Adán Sohersa <a.gonzalez@gruposohersa.com>
2022-05-04 17:38:08 -0500

    Report deliveryslip - hotfix 20

--- nirva_purchase/models/stock_picking.py
+++ nirva_purchase/models/stock_picking.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from nirva_purchase.models import purchase_order
 from odoo import models, fields, api
 
 class StockPicking(models.Model):
@@ -7,17 +8,24 @@ class StockPicking(models.Model):
     _inherit='stock.picking'
 
     # Filtramos el dominio del campo purchase_order
-    def _set_purchase_order(self):
-        domain =[('id', '=', -1)]
-        purchase_order_list=[]
-        purchase_orders = self.env['purchase.order'].search([('name','=', self.origin)])
-        for each in purchase_orders:
-            purchase_order_list.append(each.id)
-        if purchase_order_list:
-            domain =[('id', 'in', purchase_order_list)]
-            return domain
-        return domain
+    # def _set_purchase_order(self):
+    #     domain =[('id', '=', -1)]
+    #     purchase_order_list=[]
+    #     purchase_orders = self.env['purchase.order'].search([('name','=', self.origin)])
+    #     for each in purchase_orders:
+    #         purchase_order_list.append(each.id)
+    #     if purchase_order_list:
+    #         domain =[('id', 'in', purchase_order_list)]
+    #         return domain
+    #     return domain
 
     # Agregamos un campo relacional para poder acceder a la orden
     # de compra de la cuál deriva la recepción de los productos
-    purchase_order = fields.Many2one('purchase.order', string='Orden de compra', domain=_set_purchase_order)
+    purchase_order = fields.Many2one('purchase.order', string='Orden de compra')
+
+    @api.onchange('name')
+    def _set_purchase_order_domain(self):
+        for rec in self:
+            # Definimos el dominio
+            purchase_order_domain = [("name","=", rec.origin)]
+            return {'domain': {'purchase_order': purchase_order_domain}}
