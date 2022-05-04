1789019cbdae9dd2cf769b891ad3bb9fc84665b7
Adán Sohersa <a.gonzalez@gruposohersa.com>
2022-05-04 17:13:16 -0500

    Report deliveryslip - hotfix 16

--- nirva_purchase/models/stock_picking.py
+++ nirva_purchase/models/stock_picking.py
@@ -7,15 +7,17 @@ class StockPicking(models.Model):
     _inherit='stock.picking'
 
     # Filtramos el dominio del campo purchase_order
-    @api.one
+    @api.onchange("name")
     def _set_purchase_order(self):
-        #Definimos el dominio acotándolo sólo al que coincida con el nombre de origen
-        purchase_order_by_name = [('name', "=", self.origin)]
-        return {'domain': {'purchase_order': purchase_order_by_name}}
+        if (self.origin):
+            #Definimos el dominio acotándolo sólo al que coincida con el nombre de origen
+            purchase_order_by_name = [('name', "=", self.origin)]
+            # return [('name', "=", self.origin)]
+            return {'domain': {'purchase_order': purchase_order_by_name}}
 
     # Agregamos un campo relacional para poder acceder a la orden
     # de compra de la cuál deriva la recepción de los productos
-    purchase_order = fields.Many2one('purchase.order', string='Orden de compra', domain=_set_purchase_order)
+    # purchase_order = fields.Many2one('purchase.order', string='Orden de compra', domain=_set_purchase_order)
 
     # @api.multi
     # # Filtramos el dominio del campo purchase_order
