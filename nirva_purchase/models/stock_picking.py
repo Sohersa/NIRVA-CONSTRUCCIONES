496e119c25647cd1bd0c6c642c72819ea7e883be
Adán Sohersa <a.gonzalez@gruposohersa.com>
2022-05-04 18:29:58 -0500

    Report deliveryslip - hotfix 22

--- nirva_purchase/models/stock_picking.py
+++ nirva_purchase/models/stock_picking.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
 
+from nirva_purchase.models import purchase_order
 from odoo import models, fields, api
 
 class StockPicking(models.Model):
 
     _inherit='stock.picking'
 
-    # Filtramos el dominio del campo purchase_order
-    # def _set_purchase_order(self):
-    #     domain =[('id', '=', -1)]
-    #     purchase_order_list=[]
-    #     purchase_orders = self.env['purchase.order'].search([('name','=', self.origin)])
-    #     for each in purchase_orders:
-    #         purchase_order_list.append(each.id)
-    #     if purchase_order_list:
-    #         domain =[('id', 'in', purchase_order_list)]
-    #         return domain
-    #     return domain
+    # # Agregamos un campo relacional para poder acceder a la orden
+    # # de compra de la cuál deriva la recepción de los productos
+    # purchase_order = fields.Many2one('purchase.order', string='Orden de compra', default=)
 
-    # Agregamos un campo relacional para poder acceder a la orden
-    # de compra de la cuál deriva la recepción de los productos
-    purchase_order = fields.Many2one('purchase.order', string='Orden de compra')
-
-    @api.onchange('name')
+    # @api.onchange('origin')
+    # def _set_purchase_order_domain(self):
+    #     for rec in self:
+    #         # Definimos el dominio
+    #         purchase_order_domain = [("name","=", rec.origin)]
+    #         return {'domain': {'purchase_order': purchase_order_domain}}
+    @api.model
     def _set_purchase_order_domain(self):
-        for rec in self:
+        if (self.origin):
             # Definimos el dominio
-            purchase_order_domain = [("name","=", rec.origin)]
-            return {'domain': {'purchase_order': purchase_order_domain}}
+            return [("name","=", self.origin)]
+
+    @api.model
+    def _default_purchase_order(self):
+        return self.env['purchase.order'].search([('name', '=', 'origin')], limit=1)
+
+    purchase_order = fields.Many2one('purchase.order', string="Orden de compra", default=_default_purchase_order, required=True, domain=_set_purchase_order_domain)
