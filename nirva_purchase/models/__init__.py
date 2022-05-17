a179d29dbbb87b95760ac21567f6f36b9f3b94aa
Adán Sohersa <a.gonzalez@gruposohersa.com>
2022-05-17 12:20:16 -0500

    Regimen fiscal v0.0.0 - hotfix 3

--- nirva_purchase/models/__init__.py
+++ nirva_purchase/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import account_move, purchase_order, purchase_requisition, res_partner
+from . import res_partner, account_move, purchase_order, purchase_requisition
