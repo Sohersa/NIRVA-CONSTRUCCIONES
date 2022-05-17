8448292bd4411e8b6b881a880fa140c6f2c7f00d
Adán Sohersa <a.gonzalez@gruposohersa.com>
2022-05-17 12:12:55 -0500

    Regimen fiscal v0.0.0 - hotfix 1

--- nirva_purchase/models/account_move.py
+++ nirva_purchase/models/account_move.py
@@ -7,4 +7,14 @@ class AccountMove(models.Model):
 
     ref = fields.Char(string="Referenia UUID")
 
-    regimen_fiscal = fields.Selection([('Asalariados','Servicios profesionales (honorarios)','Arrendamiento de inmuebles','Actividad empresarial','Incorporación fiscal','General','Personas morales con fines no lucrativos')], string="Regimen Fiscal", related="partner_id.regimen_fiscal")
+    def _regimenes_fiscales():
+        return [
+            ('Asalariados', 'Asalariados'), 
+            ('Servicios profesionales (honorarios)','Servicios profesionales (honorarios)'),
+            ('Arrendamiento de inmuebles', 'Arrendamiento de inmuebles'),
+            ('Actividad empresarial', 'Actividad empresarial'),
+            ('Incorporación fiscal', 'Incorporación fiscal'),
+            ('General', 'General'),
+            ('Personas morales con fines no lucrativos' 'Personas morales con fines no lucrativos')
+        ]
+    regimen_fiscal = fields.Selection(selection='_regimenes_fiscales', string="Regimen Fiscal", related="partner_id.regimen_fiscal")
