2a961d8338add1151bf9ea315938b65ea0743bbb
eConstruction-dev <econstruction.dev@gmail.com>
2022-08-16 10:23:34 -0500

    Stock location - warehouse update v0.0.1

--- nirva_purchase/models/stock_location.py
+++ nirva_purchase/models/stock_location.py
@@ -9,16 +9,6 @@ class StockLocation(models.Model):
     is_contract = fields.Boolean(string="Es un contrato")
     warehouse_id = fields.Many2one('stock.warehouse', string='Almacen/Obra', required=False)
 
-    # Establecemos el nombre de la ubiciación al cambiar el campo de warehouse
-    @api.onchange('warehouse_id')
-    def _onchange_warehouse(self):
-        for rec in self:
-            # Si hay una almacén establecido...
-            if(rec.warehouse_id):
-                # Agregamos el nombre del almacén al nombre de la ubicación
-                rec['name'] = rec.warehouse_id.name + '/CONTRATO ' + rec['name']
-                return
-
     def _set_is_contract(self):
         for location in self:
             if not location.location_id and location.usage == "transit":
