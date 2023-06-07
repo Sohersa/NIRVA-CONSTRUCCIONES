2367e2edbf0ecb519d16f1d7ec0dac00155a3530
eConstruction-dev <econstruction.dev@gmail.com>
2023-06-07 09:53:25 -0600

    [AccountMove] Filtering partner_bank_id's domain by partner_id.parent_id or by partner_id - v0.0.6

--- nirva_purchase/models/account_move.py
+++ nirva_purchase/models/account_move.py
@@ -33,7 +33,8 @@ class AccountMove(models.Model):
     def _onchange_partner(self):
         for rec in self:
             # Establecemos la primer cuenta de la tupla en el campo correspondiente
-            rec.partner_bank_id = False
+            rec['bank_partner_id'] = False
+            rec['partner_bank_id'] = False
             
             # Si hay un partner establecido
             if (rec.partner_id):
